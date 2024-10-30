4af272617cdbaed8363f0c8ec8652310ccdd02fb
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-30 16:33:24 +0500

    console removed

--- Assets/javaScript/view/footer.js
+++ Assets/javaScript/view/footer.js
@@ -1,4 +1,3 @@
 let footer = document.querySelector(".footerText");
-let year = new Date().getFullYear()
-footer.innerHTML = `&copy; Codesty Games | <span>${year}</span>`
-console.log(footer)
+let year = new Date().getFullYear();
+footer.innerHTML = `&copy; Codesty Games | <span>${year}</span>`;
