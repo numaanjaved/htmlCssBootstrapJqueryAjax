4b039da138e276f39d629b40598eea70bb31a037
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-30 16:36:34 +0500

    footer converted to jQuery

--- Assets/javaScript/view/footer.js
+++ Assets/javaScript/view/footer.js
@@ -1,3 +1,2 @@
-let footer = document.querySelector(".footerText");
 let year = new Date().getFullYear();
-footer.innerHTML = `&copy; Codesty Games | <span>${year}</span>`;
+$(".footerText").html(`&copy; Codesty Games | <span>${year}</span>`);
