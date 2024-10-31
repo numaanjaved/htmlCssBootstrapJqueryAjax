de2464ae382435e839ee728450015cc9067d8388
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-10-31 16:25:18 +0500

    Limit check functionality is working

---
+++ Assets/javaScript/views/homeView/form/charLimitCheck.js
@@ -0,0 +1,13 @@
+let charLimitCheck = () => {
+    userBio.on("input", (e) => {
+        let userBioVal = e.target.value;
+        if (userBioVal.length > 300) {
+            bioLimitExceed.html(`Character Limit Exceeded`);
+            userBio.css({ "outline": "1px solid #a31b1b", "border": "2px solid #a31b1b" });
+        } else {
+            userBio.css({ "outline": "1px solid #90c5cf", "border": "2px solid #90c5cf" });
+            bioLimitExceed.html(``);
+        }
+    })
+}
+charLimitCheck();
