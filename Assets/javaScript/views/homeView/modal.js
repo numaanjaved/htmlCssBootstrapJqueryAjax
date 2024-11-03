06a28ea19a7f10e7ce79c963b4dd4c3cafe06553
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-11-03 15:12:40 +0500

    Modal converted to Bootstrap

--- Assets/javaScript/views/homeView/modal.js
+++ Assets/javaScript/views/homeView/modal.js
@@ -1,7 +1,7 @@
 import { usersDataMainContainer } from "./record.js";
 let modal = ([profilePic, userName, userEmail, userContactNum, userAddress, userBio, id = null]) => {
-    let modalMainContainer = createNewElement(["section", "user_modal", usersDataMainContainer, null, { id: `${id}` }]);
-    let modalImageContainer = createNewElement(["div", "image_container", modalMainContainer]);
+    let modalMainContainer = createNewElement(["section", "user_modal flex-row justify-content-between align-items-center", usersDataMainContainer, null, { id: `${id}` }]);
+    let modalImageContainer = createNewElement(["div", "image_container d-flex justify-content-center align-items-center flex-column", modalMainContainer]);
     let modalPicFigureTag = createNewElement(["figure", "profile_figure_tag d-flex justify-content-center align-items-center", modalImageContainer]);
     createNewElement(["img", null, modalPicFigureTag, null, { src: `${profilePic}` }]);
     createNewElement(["p", "profile_info", modalImageContainer, `Profile ID: ${id}`]);
@@ -19,10 +19,11 @@ let modal = ([profilePic, userName, userEmail, userContactNum, userAddress, user
     let btnContainer = createNewElement(["div", "modal_btn_container w-100 d-flex justify-content-center align-items-center", modalTextContainer]);
     let modalCloseBtn = createNewElement(["button", "modal_close_btn btn btn-danger", btnContainer, `Close`]);
     $(modalCloseBtn).click(() => {
+        console.log(`hi`)
         let records = $(`.individual_user_data`);
         $.each(records, (record) => { $(record).css("filter", "blur(0px)"); });
         $(modalMainContainer).css("display", "none");
         // usersDataMainContainer.style.minHeight = "";
     });
 };
-modal(["./Assets/images/image5.png", "Ahmed", "AHmed@gmaill.com", "03123123124", "Chakwal", `Tech Person `, `prof001`]);
+modal(["./Assets/images/image5.png", "Ahmed", "AHmed@gmaill.com", "03123123124", "Chakwal", `Tech Person `, `prof001`]);    
