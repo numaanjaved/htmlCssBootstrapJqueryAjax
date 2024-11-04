67f0641e904a1ad17f3bb6d6ce0a7ed0ba395dbe
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-11-04 11:54:54 +0500

    Is Null Is Working on All Attributes

--- Assets/javaScript/controllers/formValidation.js
+++ Assets/javaScript/controllers/formValidation.js
@@ -3,9 +3,13 @@ import { User } from "../models/User.js";
 export let formValidation = () => {
     let validationCheck = true;
     let newUser = new User();
-    if (!newUser.isNull(userFirstName)) {
-        validationCheck = false;
-    }
+    if (!newUser.isNull(userFirstName)) { validationCheck = false; }
+    if (!newUser.isNull(userLastName)) { validationCheck = false; }
+    if (!newUser.isNull(userEmail)) { validationCheck = false; }
+    if (!newUser.isNull(userContactNumber)) { validationCheck = false; }
+    if (!newUser.isNull(userAddress)) { validationCheck = false; }
+    if (!newUser.isNull(userBio)) { validationCheck = false; }
+
     if (validationCheck) {
         console.log(`Validation Successful`);
     } else {
--- Assets/javaScript/views/homeView/form/charLimitCheck.js
+++ Assets/javaScript/views/homeView/form/charLimitCheck.js
@@ -1,13 +1,13 @@
-import { userBio } from "../elementReferences.js";
+import { userBio, bioLimitExceed } from "../elementReferences.js";
 let charLimitCheck = () => {
     userBio.on("input", (e) => {
         let userBioVal = e.target.value;
         if (userBioVal.length > 300) {
-            bioLimitExceed.html(`Character Limit Exceeded`);
-            userBio.css({ "outline": "1px solid #a31b1b", "border": "2px solid #a31b1b" });
+            $(bioLimitExceed).html(`Character Limit Exceeded`);
+            $(userBio).css({ "outline": "1px solid #a31b1b", "border": "2px solid #a31b1b" });
         } else {
-            userBio.css({ "outline": "1px solid #90c5cf", "border": "2px solid #90c5cf" });
-            bioLimitExceed.html(``);
+            $(userBio).css({ "outline": "1px solid #90c5cf", "border": "2px solid #90c5cf" });
+            $(bioLimitExceed).html(``);
         }
         $(`.limit_text`).html(`${userBioVal.length}/300`);
     })
