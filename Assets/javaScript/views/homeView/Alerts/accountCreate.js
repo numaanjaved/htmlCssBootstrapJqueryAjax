d25f9b0ad5280efd2d5d122412ae018ff33bacf3
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-11-13 15:24:53 +0500

    Try catch added on alerts

--- Assets/javaScript/views/homeView/Alerts/accountCreate.js
+++ Assets/javaScript/views/homeView/Alerts/accountCreate.js
@@ -1,17 +1,20 @@
 import { createNewElement } from "../../components/createElement.js";
+import { errorNotification } from "./errorNotification.js";
 
 let createAccCounter = 0;
 export let accountCreated = (src) => {
-    createAccCounter += 1;
-    let notificationContainer = $("<div></div>");
-    notificationContainer.addClass(`notification_container successCont${createAccCounter}`);
-    $(".notifications_container").prepend(notificationContainer);
-    let pictureContainer = createNewElement(["div", `picture_container picContainer${createAccCounter}`, `.successCont${createAccCounter}`]);
-    let figureTag = createNewElement(["figure", `greenBg greenBorder figTag${createAccCounter}`, `.picContainer${createAccCounter}`]);
-    createNewElement(["img", "notificationPic", `.figTag${createAccCounter}`, null, { src: src, alt: "User Profile Picture", id: "notification_Img" }]);
-    let textContainer = createNewElement(["div", `greenBg greenBorder text_container textCont${createAccCounter}`, `.successCont${createAccCounter}`]);
-    createNewElement(["h3", "notification_heading", `.textCont${createAccCounter}`, `Profile Created Successfully!`]);
-    createNewElement(["button", "notification_close_btn notificationClose", `.textCont${createAccCounter}`, `<i class="fa-solid fa-circle-xmark"></i>`, { id: "successNotificationCloseBtn", type: "button" }]);
+    try {
+        createAccCounter += 1;
+        let notificationContainer = $("<div></div>");
+        notificationContainer.addClass(`notification_container successCont${createAccCounter}`);
+        $(".notifications_container").prepend(notificationContainer);
+        let pictureContainer = createNewElement(["div", `picture_container picContainer${createAccCounter}`, `.successCont${createAccCounter}`]);
+        let figureTag = createNewElement(["figure", `greenBg greenBorder figTag${createAccCounter}`, `.picContainer${createAccCounter}`]);
+        createNewElement(["img", "notificationPic", `.figTag${createAccCounter}`, null, { src: src, alt: "User Profile Picture", id: "notification_Img" }]);
+        let textContainer = createNewElement(["div", `greenBg greenBorder text_container textCont${createAccCounter}`, `.successCont${createAccCounter}`]);
+        createNewElement(["h3", "notification_heading", `.textCont${createAccCounter}`, `Profile Created Successfully!`]);
+        createNewElement(["button", "notification_close_btn notificationClose", `.textCont${createAccCounter}`, `<i class="fa-solid fa-circle-xmark"></i>`, { id: "successNotificationCloseBtn", type: "button" }]);
+    } catch (error) { errorNotification(`Error in account created notification`); }
 };
 
 $(document).on("click", "#successNotificationCloseBtn", (e) => {
--- Assets/javaScript/views/homeView/Alerts/errorNotification.js
+++ Assets/javaScript/views/homeView/Alerts/errorNotification.js
@@ -2,13 +2,15 @@ import { createNewElement } from "../../components/createElement.js";
 let containerId = 0;
 
 export let errorNotification = (errorMsg) => {
-    containerId += 1;
-    let errNotificationContainer = $("<div></div>");
-    errNotificationContainer.addClass(`err_notification_container cont${containerId}`);
-    $(".notifications_container").prepend(errNotificationContainer);
-    createNewElement(["div", `err_text_container errorCustom errTextCont${containerId}`, `.cont${containerId}`]);
-    createNewElement(["h3", "notification_err_heading", `.errTextCont${containerId}`, errorMsg]);
-    createNewElement(["button", "err_notification_close_btn", `.errTextCont${containerId}`, `<i i class="fa-solid fa-circle-xmark" ></i > `, { id: `errNotificationCloseBtn`, type: "button" }]);
+    try {
+        containerId += 1;
+        let errNotificationContainer = $("<div></div>");
+        errNotificationContainer.addClass(`err_notification_container cont${containerId}`);
+        $(".notifications_container").prepend(errNotificationContainer);
+        createNewElement(["div", `err_text_container errorCustom errTextCont${containerId}`, `.cont${containerId}`]);
+        createNewElement(["h3", "notification_err_heading", `.errTextCont${containerId}`, errorMsg]);
+        createNewElement(["button", "err_notification_close_btn", `.errTextCont${containerId}`, `<i i class="fa-solid fa-circle-xmark" ></i > `, { id: `errNotificationCloseBtn`, type: "button" }]);
+    } catch (error) { errorNotification(`Error in error notification`); }
 };
 
 $(document).on("click", "#errNotificationCloseBtn", (e) => {
