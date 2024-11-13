dd2b1b30e417d9532fedce8978dc098245f956be
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-11-13 17:32:11 +0500

    Update notification is working

--- Assets/javaScript/views/homeView/Alerts/accountUpdate.js
+++ Assets/javaScript/views/homeView/Alerts/accountUpdate.js
@@ -1,12 +1,13 @@
 import { createNewElement } from "../../components/createElement.js";
 import { errorNotification } from "./errorNotification.js";
-
+import { removeNotification } from "./remove.js";
 let updateAccCounter = 0;
 export let accountUpdated = (src) => {
     try {
         updateAccCounter += 1;
         let notificationContainer = $("<div></div>");
         notificationContainer.addClass(`notification_container updateCont${updateAccCounter}`);
+        $(notificationContainer).attr("id", `updateNotificationContainer${updateAccCounter}`);
         $(".notifications_container").prepend(notificationContainer);
         let pictureContainer = createNewElement(["div", `picture_container updatePicContainer${updateAccCounter}`, `.updateCont${updateAccCounter}`]);
         let figureTag = createNewElement(["figure", `updateFigTag${updateAccCounter} blueBg blueBorder`, `.updatePicContainer${updateAccCounter}`]);
@@ -14,6 +15,7 @@ export let accountUpdated = (src) => {
         let textContainer = createNewElement(["div", `text_container blueBg blueBorder updateTextCont${updateAccCounter}`, `.updateCont${updateAccCounter}`]);
         createNewElement(["h3", "notification_heading", `.updateTextCont${updateAccCounter}`, `Profile Updated Successfully!`]);
         createNewElement(["button", "notification_close_btn notificationClose", `.updateTextCont${updateAccCounter}`, `<i class="fa-solid fa-circle-xmark"></i>`, { id: "updateNotificationCloseBtn", type: "button" }]);
+        removeNotification($(notificationContainer).attr("id"));
     } catch (error) { errorNotification(`Error in account update notification`); }
 };
 
