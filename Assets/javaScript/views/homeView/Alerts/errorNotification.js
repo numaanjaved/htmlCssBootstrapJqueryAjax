59787dbd602927688ef14751043442194f63c3e8
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-11-13 17:27:02 +0500

    Error Notification Automatically removing

--- Assets/javaScript/views/homeView/Alerts/errorNotification.js
+++ Assets/javaScript/views/homeView/Alerts/errorNotification.js
@@ -1,14 +1,17 @@
 import { createNewElement } from "../../components/createElement.js";
+import { removeNotification } from "./remove.js";
 let containerId = 0;
 
 export let errorNotification = (errorMsg) => {
     containerId += 1;
     let errNotificationContainer = $("<div></div>");
     errNotificationContainer.addClass(`err_notification_container cont${containerId}`);
+    $(errNotificationContainer).attr("id", `errNotificationContainer${containerId}`);
     $(".notifications_container").prepend(errNotificationContainer);
     createNewElement(["div", `err_text_container errorCustom errTextCont${containerId}`, `.cont${containerId}`]);
     createNewElement(["h3", "notification_err_heading", `.errTextCont${containerId}`, errorMsg]);
     createNewElement(["button", "err_notification_close_btn", `.errTextCont${containerId}`, `<i i class="fa-solid fa-circle-xmark" ></i > `, { id: `errNotificationCloseBtn`, type: "button" }]);
+    removeNotification($(errNotificationContainer).attr("id"));
 };
 
 $(document).on("click", "#errNotificationCloseBtn", (e) => {
