1db55534e23533767f644391930ff8488f40b2e0
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-11-13 17:15:56 +0500

    Account Delete notification working

--- Assets/javaScript/views/homeView/Alerts/accountDelete.js
+++ Assets/javaScript/views/homeView/Alerts/accountDelete.js
@@ -1,12 +1,13 @@
 import { createNewElement } from "../../components/createElement.js";
 import { errorNotification } from "./errorNotification.js";
-
+import { removeNotification } from "./remove.js";
 let deleteAccCounter = 0;
 export let accountDeleted = (src) => {
     try {
         deleteAccCounter += 1;
         let notificationContainer = $("<div></div>");
         notificationContainer.addClass(`notification_container deleteCont${deleteAccCounter}`);
+        $(notificationContainer).attr("id", `delNotificationContainer${deleteAccCounter}`);
         $(".notifications_container").prepend(notificationContainer);
         let pictureContainer = createNewElement(["div", `picture_container delPicContainer${deleteAccCounter}`, `.deleteCont${deleteAccCounter}`]);
         let figureTag = createNewElement(["figure", `redBg redBorder delFigTag${deleteAccCounter}`, `.delPicContainer${deleteAccCounter}`]);
@@ -14,6 +15,7 @@ export let accountDeleted = (src) => {
         let textContainer = createNewElement(["div", `redBg redBorder text_container delTextCont${deleteAccCounter}`, `.deleteCont${deleteAccCounter}`]);
         createNewElement(["h3", "notification_heading", `.delTextCont${deleteAccCounter}`, `Profile Deleted Successfully!`]);
         createNewElement(["button", "notification_close_btn notificationClose", `.delTextCont${deleteAccCounter}`, `<i class="fa-solid fa-circle-xmark"></i>`, { id: "accDelNotificationCloseBtn", type: "button" }]);
+        removeNotification($(notificationContainer).attr("id"));
     } catch (error) { errorNotification(`Error in account Deleted Notification`); }
 };
 
---
+++ Assets/javaScript/views/homeView/Alerts/remove.js
@@ -0,0 +1,7 @@
+export let removeNotification = (id) => {
+    setTimeout(() => {
+        try {
+            $(`#${id}`).remove();
+        } catch (error) { removeNotification(`Error occurred while removing notification`) }
+    }, 60000)
+}
